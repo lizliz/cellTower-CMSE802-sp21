baf6f6ca91c9196102aa1aced517ba3538436ef2
sai-siril <g.saisiril@gmail.com>
2021-02-17 10:05:52 -0500

    Added coloring funciton, grp 3

--- GreedyGraphColoring.py
+++ GreedyGraphColoring.py
@@ -5,10 +5,11 @@ from random import randrange
 #Colouring Stub function
 def GreedyGraphColoring(graph, M):
     #take an empty array
-    #for each node in graph, randomly assign a color  
+    #for each node in graph, randomly assign a color 
+    #color is integer in our case    
     colour_map = []
     for node in graph:
-        colour_map.append(np.random.uniform(0,M))
+        colour_map.append(np.random.randint(0,M))
                           
     return colour_map
-    
+  
